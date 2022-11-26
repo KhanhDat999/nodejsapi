924bd8353e81310e8e2c1d0bf95ce13093ff9e37
admin <admin@MacBook-Po-ro-cua-at-Khet-Let.local>
2022-11-26 19:37:19 +0700

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   database.query( `SELECT * FROM test` ,(err , data) => {
     if (err) throw err;
-    res.json("home");
+    res.json(data);
   });
 }),
 
